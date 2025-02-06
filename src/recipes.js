64fd56d1a17ac72565d6594c8de46a285d6aa324
apaajaa45 <apa204000@gmail.com>
2025-02-06 13:44:09 +0700

    Add files via upload

---
+++ src/recipes.js
@@ -0,0 +1,31 @@
+const recipes = [
+    { 
+      id: 1,
+      title: "Greek salad",
+      price: 12.99,
+      image: "https://i.ibb.co/68c9bLv/greek-salad.jpg",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis esse recusandae, aspernatur neque odit corrupti saepe sunt, atque non molestias quod nostrum pariatur commodi vitae consequatur voluptatum cupiditate doloremque. Tenetur",
+    },
+    {
+      id: 2,
+      title: "Bruchetta",
+      price: 5.99,
+      image: "https://i.ibb.co/317GqZv/Bruchetta.png",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis esse recusandae, aspernatur neque odit corrupti saepe sunt, atque non molestias quod nostrum pariatur commodi vitae consequatur voluptatum cupiditate doloremque. Tenetur.",
+    },
+    {
+      id: 3,
+      title: "Lemon Dessert",
+      price: 4.78,
+      image: "https://i.ibb.co/sm6MfcH/desert.jpg",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis esse recusandae, aspernatur neque odit corrupti saepe sunt, atque non molestias quod nostrum pariatur commodi vitae consequatur voluptatum cupiditate doloremque. Tenetur.",
+    },
+  
+  ];
+  
+  export default recipes;
+  
+  
